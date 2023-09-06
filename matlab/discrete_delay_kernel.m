9eb398a8e39eb402dbf9050fb3b777d0f16425b4
aslik97 <aslik97@hotmail.com>
2023-09-06 02:57:52 -0300

    updates

--- matlab/discrete_delay_kernel.m
+++ matlab/discrete_delay_kernel.m
@@ -1,14 +1,10 @@
 
 
 % Define parameters
-mu = 3.9 * exp(-5);
+mu = 3.9 * 10^(-5);
 v = 1/7;
 beta = 10 * (mu + v);
 alpha = 0.002;
-tau_0 = 2;
-
-% Define the Laplace transform of the kernel
-Lg = @(lambda) exp(-lambda * tau_0);
 
 % Define the range of k and tau values
 k_values = 0.1:0.1:500;
@@ -23,33 +19,17 @@ for i = 1:length(k_values)
     for j = 1:length(tau_values)
         tau = tau_values(j);
 
-        % Calculate characteristic equation coefficients
-        E3_S = (mu + v) / beta;
-        E3_I = (mu * alpha * (beta - (mu + v))) / (beta * (mu + alpha * (mu + v)));
-        E3_p = (mu * (beta - (mu + v))) / (beta * (mu + alpha * (mu + v)));
+        % steady state
+        S_star = (mu + v) / beta;
+        I_star = (mu * alpha * (beta - (mu + v))) / (beta * (mu + alpha * (mu + v)));
+        p_star = (mu * (beta - (mu + v))) / (beta * (mu + alpha * (mu + v)));
+        
+        % Calculate coefficients
+        A = 1;
+        B = (beta * I_star + mu)^2 - 2 * beta^2 * S_star * I_star - (k * alpha * p_star * (1 - p_star))^2;
+        C = (beta^2 * S_star * I_star)^2 - 2 * k * mu * beta * p_star * (1 - p_star) * I_star * (beta * I_star + mu) ...
+            - (k * alpha * p_star * (1 - p_star))^2 * ((beta * I_star + mu)^2 - 2 * beta^2 * S_star * I_star);
+        D = (k * beta * p_star * (1 - p_star) * I_star)^2 * (mu^2 - alpha^2 * beta^2 * S_star^2);
 
-        % Calculate characteristic equation coefficients
-        a = k * mu * beta * E3_p * (1 - E3_p) * E3_I;
-        b = k * alpha * E3_p * (1 - E3_p) * Lg(1i) + 1i;
-        c = [1, (mu + beta * E3_I), beta^2 + E3_S * E3_I];
-
-        % Calculate eigenvalues of the characteristic equation
-        eigenvalues = roots(c);
-
-        % Check eigenvalues for stability
-        if all(real(eigenvalues) < 0)
-            stability_matrix(i, j) = 1;  % Stable
-        else
-            stability_matrix(i, j) = 0;  % Unstable
-        end
-    end
-end
-
-% Plot the regions of stability and instability
-figure;
-imagesc(tau_values, k_values, stability_matrix');
-colormap([1 0 0; 0 1 0]); % Red for unstable, green for stable
-colorbar('Ticks', [0, 1], 'TickLabels', {'Unstable', 'Stable'});
-xlabel('\tau');
-ylabel('k');
-title('Stability Analysis for E3 Steady State for discrete kernel');
+        % Calculate roots of the cubic equation
+        roots_Cubic = roots([A, 0, B, 0, C, 0, D]);
