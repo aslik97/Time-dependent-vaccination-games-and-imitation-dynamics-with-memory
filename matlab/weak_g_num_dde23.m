a7d2e9b203bc9d3576dfbea9b1ca988b656f1b15
aslik97 <aslik97@hotmail.com>
2023-10-15 02:01:00 -0300

    CORRECT OUTPUTS FOR WEAK GAMMA NUM SIM

--- matlab/weak_g_num_dde23.m
+++ matlab/weak_g_num_dde23.m
@@ -1,13 +1,16 @@
+%CORRECT OUTPUTS
+
 clear all:
 close all;
 % define vector with all delays
 lags=[20,50,100,150];
-%lags=[20];
+%lags=[50];
+
 for j = 1:length(lags)
         tau = lags(j);
- %options = ddeset('RelTol',1e-6,'AbsTol',1e-8);
+%options = ddeset('RelTol',1e-6,'AbsTol',1e-8);
 solution = dde23(@(t,x,Z) rhs_dde23(t,x,Z,tau),tau,@dde23history, [0,4000]);
-disp(size(solution.y(2,:)));
+disp(size(solution.y));
 % plot the results
 figure;
    plot(solution.x,solution.y(2,:),'r-','Linewidth',2)
@@ -19,8 +22,7 @@ hold off
 end
 
 function x_h = dde23history(t)
-    %mu = 3.9 * 10^(-5);
-     mu=1/(70*365);
+    mu = 3.9 * 10^(-5);
     v = 1/7;
     R0= 10;
 
@@ -28,14 +30,14 @@ function x_h = dde23history(t)
     p = 0;
     I = mu * (1 - 1/R0) / (mu + v);
     S = 1/R0;
-    disp(I);
-   
-   % if t == 0
-   %    a = 0.1;      % Control the steepness. Adjust as needed.
-   % t0 = 0;      % The time when p = 0.5. Adjust as needed.
-   % p = 1 / (1 + exp(-a * (t - t0)));
-    
-   % end
+
+    % For t >= 0
+    %if t >= 0
+    %    a = 0.1;      % Control the steepness. Adjust as needed.
+    %t0 = 0;      % The time when p = 0.5. Adjust as needed.
+    %p = 1 / (1 + exp(-a * (t - t0)));
+    %disp(p);
+    %end
    
     x_h = [S I p];
 end
@@ -43,34 +45,23 @@ end
 
 function xdot = rhs_dde23(t,x,Z,tau)
     k = 400;
-    %mu = 3.9 * 10^(-5);
-    mu=1/(70*365);
+    mu = 3.9 * 10^(-5);
     v = 1/7;
-    R0=10;
-    beta = R0 * (mu + v);
+    beta = 10 * (mu + v);
     alpha = 0.002;
-   
+   sigma = 1/tau;
     % Assuming Z corresponds to delayed values of dynamicasl system at time t-tau
-    p_delayed = Z(3); % delayed p for the third differential equation
-    
-    % Discrete delay using Dirac delta
-    %if t < tau
-    %    integral_term = 0;
-    %else
-        integral_term = p_delayed;
-    %end
+    p_delayed = Z(3,1); % delayed p for the third differential equation
+    disp(['p_delayed: ', num2str(p_delayed)]);
+   % Using the weak gamma kernel
+    g = sigma * exp(-sigma * tau);
+    integral_term = g * p_delayed; % This models the convolution with the kernel
+    %disp(integral_term);
     xdot = [
         mu * (1 - x(3)) - beta * x(1) * x(2) - mu * x(1) ;
         beta * x(1) * x(2) - (mu + v) * x(2);
         k * x(3) * (1 - x(3)) * (1 - alpha * integral_term);% Here, delayed p directly
     ];
-   % disp(['Time: ', num2str(t), ' Delayed Time: ', num2str(t-tau), 'p_delayed: ', num2str(p_delayed)]);
+   disp(['Time: ', num2str(t), ' Delayed Time: ', num2str(t-tau)]);
+
 end
-
-
-%function f = f_function(p_delayed,tau,t)% Gaussian approximation
-%sigma = 0.01;  % Small standard deviation
-%dirac_approximation = 1 / (sigma * sqrt(2 * pi)) * exp(-(t- tau).^2 / (2 * sigma^2));
-%f= p_delayed * dirac_approximation;
-
-%end
